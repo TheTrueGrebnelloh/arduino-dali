a6e037e097393e5480aa855a2cd39acb1cf3d1e5
Timo Tolksdorf ENSOBIT <tt@ensobit.de>
2025-08-10 12:16:26 +0200

    add fade time to dali helper

--- src/DaliHelper.h
+++ src/DaliHelper.h
@@ -24,4 +24,59 @@ class DaliHelper
             return pow(10, ((arc-1) / (253/3.0)) - 1);
         }
 
+        static uint32_t fadeTimeFromNumber(uint8_t number) {
+            switch (number)
+            {
+            case 0:
+                return 700;
+                break;
+            case 1:
+                return 1000;
+                break;
+            case 2:
+                return 1400;
+                break;
+            case 3:
+                return 2000;
+                break;
+            case 4:
+                return 2800;
+                break;
+            case 5:
+                return 4000;
+                break;
+            case 6:
+                return 5700;
+                break;
+            case 7:
+                return 8000;
+                break;
+            case 8:
+                return 11300;
+                break;
+            case 9:
+                return 16000;
+                break;
+            case 10:
+                return 22600;
+                break;
+            case 11:
+                return 32000;
+                break;
+            case 12:
+                return 45300;
+                break;
+            case 13:
+                return 64000;
+                break;
+            case 14:
+                return 90500;
+                break;
+            default:
+                Serial.printf("Got unknown fade time number %u\n", number);
+                return 100;
+                break;
+            }
+        }
+
 };
