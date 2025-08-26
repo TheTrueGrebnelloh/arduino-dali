8a6fc0358dc8c1003c39977e32cb779ff3b6c0d3
Timo Tolksdorf ENSOBIT <tt@ensobit.de>
2025-08-26 21:25:42 +0200

    add fade rate and fix fade time in dali helper

--- src/DaliHelper.h
+++ src/DaliHelper.h
@@ -28,48 +28,51 @@ class DaliHelper
             switch (number)
             {
             case 0:
-                return 700;
+                return 50;
                 break;
             case 1:
-                return 1000;
+                return 700;
                 break;
             case 2:
-                return 1400;
+                return 1000;
                 break;
             case 3:
-                return 2000;
+                return 1400;
                 break;
             case 4:
-                return 2800;
+                return 2000;
                 break;
             case 5:
-                return 4000;
+                return 2800;
                 break;
             case 6:
-                return 5700;
+                return 4000;
                 break;
             case 7:
-                return 8000;
+                return 5700;
                 break;
             case 8:
-                return 11300;
+                return 8000;
                 break;
             case 9:
-                return 16000;
+                return 11300;
                 break;
             case 10:
-                return 22600;
+                return 16000;
                 break;
             case 11:
-                return 32000;
+                return 22600;
                 break;
             case 12:
-                return 45300;
+                return 32000;
                 break;
             case 13:
-                return 64000;
+                return 45300;
                 break;
             case 14:
+                return 64000;
+                break;
+            case 15:
                 return 90500;
                 break;
             default:
@@ -79,4 +82,61 @@ class DaliHelper
             }
         }
 
+        static float fadeRateFromNumber(uint8_t number) {
+            switch (number)
+            {
+            case 0:
+                return 358;
+                break;
+            case 1:
+                return 253;
+                break;
+            case 2:
+                return 179;
+                break;
+            case 3:
+                return 127;
+                break;
+            case 4:
+                return 89.4;
+                break;
+            case 5:
+                return 63.3;
+                break;
+            case 6:
+                return 44.7;
+                break;
+            case 7:
+                return 31.6;
+                break;
+            case 8:
+                return 22.4;
+                break;
+            case 9:
+                return 15.8;
+                break;
+            case 10:
+                return 11.2;
+                break;
+            case 11:
+                return 7.9;
+                break;
+            case 12:
+                return 5.6;
+                break;
+            case 13:
+                return 4.0;
+                break;
+            case 14:
+                return 2.8;
+                break;
+            case 15:
+                return 1.4;
+                break;
+            default:
+                Serial.printf("Got unknown fade rate number %u\n", number);
+                return 10;
+                break;
+            }
+        }
 };
